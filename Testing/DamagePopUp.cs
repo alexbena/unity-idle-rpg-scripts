3a12e16b327825270373f255bea835316190ff97
alex <alexbenacruz@gmail.com>
2021-03-21 10:36:23 +0100

    backup, dmg pop up

---
+++ Testing/DamagePopUp.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class DamagePopUp : MonoBehaviour
+{
+
+    private TextMeshPro text_mesh;
+
+    private void Awake()
+    {
+        text_mesh = transform.GetComponent<TextMeshPro>();
+    }
+    public void Setup(int damage_amount) 
+    {
+        text_mesh.SetText(damage_amount.ToString());
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- Testing/Testing.cs
+++ Testing/Testing.cs
@@ -4,10 +4,16 @@ using UnityEngine;
 
 public class Testing : MonoBehaviour
 {
+
+    [SerializeField] private Transform popup_dmg;
     // Start is called before the first frame update
     void Start()
     {
-        
+        Transform damage_pop_transform = Instantiate(popup_dmg, Vector3.zero, Quaternion.identity);
+
+        DamagePopUp damage_popup_script = damage_pop_transform.GetComponent<DamagePopUp>();
+
+        damage_popup_script.Setup(200);
     }
 
     // Update is called once per frame
